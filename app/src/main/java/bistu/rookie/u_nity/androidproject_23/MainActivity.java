e7706535ffaa4b990b91214250b8d7501f25203d
U-nity <728501875@qq.com>
2016-09-28 22:13:41 +0800

    Revise

--- app/src/main/java/bistu/rookie/u_nity/androidproject_23/MainActivity.java
+++ app/src/main/java/bistu/rookie/u_nity/androidproject_23/MainActivity.java
@@ -1,7 +1,12 @@
 package bistu.rookie.u_nity.androidproject_23;
 
+import android.database.ContentObserver;
+import android.os.Handler;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getContentResolver().registerContentObserver(ContactsContract.Contacts.CONTENT_URI,true,new ContactsObserver(new Handler()));
     }
+
+    private final class ContactsObserver extends ContentObserver {
+        public ContactsObserver(Handler handler){
+            super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            Log.i("MonitorLog", "Something Changed!");
+        }
+    }
+
 }
